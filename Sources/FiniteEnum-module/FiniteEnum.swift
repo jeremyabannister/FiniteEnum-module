9e2ecc229e953780d66ce7e2bb156bdfdae6c2ef
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-05-29 21:02:37 +0200

    Added `FiniteEnum`.

---
+++ Sources/FiniteEnum-module/FiniteEnum.swift
@@ -0,0 +1,15 @@
+//
+//  FiniteEnum.swift
+//  
+//
+//  Created by Jeremy Bannister on 5/29/23.
+//
+
+///
+@_exported import ProperValueType_module
+
+
+///
+public protocol FiniteEnum:
+    CaseIterable,
+    ProperValueType { }
--- Sources/FiniteEnum-module/FiniteEnum_module.swift
+++
@@ -1,6 +0,0 @@
-public struct FiniteEnum_module {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
