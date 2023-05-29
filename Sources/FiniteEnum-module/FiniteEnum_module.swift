fd4caa159b5c1eb6c82d2da500e21ec5467e61ee
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-05-29 20:59:05 +0200

    Initial Commit

---
+++ Sources/FiniteEnum-module/FiniteEnum_module.swift
@@ -0,0 +1,6 @@
+public struct FiniteEnum_module {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/FiniteEnum-moduleTests/FiniteEnum_moduleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FiniteEnum_module
+
+final class FiniteEnum_moduleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FiniteEnum_module().text, "Hello, World!")
+    }
+}
